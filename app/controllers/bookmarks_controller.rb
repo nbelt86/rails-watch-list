a5956410d2008d373bde341d17e8b25db3fe63c0
Nathan Beltran nbelt86@gmail.com <nbelt86@gmail.com>
2026-01-20 20:40:48 +0900

    accout creation and name

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,22 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark= Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require "open-uri"
+require "json"
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +10,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+puts "Fetching movies from TMDB..."
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)["results"]
+
+movies.first(20).each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmbd.org/t/p/w500#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Done!"
