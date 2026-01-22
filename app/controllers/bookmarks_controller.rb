dfb817719449592703016900e452397070cf0054
Nathan Beltran nbelt86@gmail.com <nbelt86@gmail.com>
2026-01-22 19:35:08 +0900

    added bookmarks plus delete function

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,15 +9,22 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to bookmark_path(@bookmark)
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
+  end
+
   private
 
-  def bookmark_params_params
+  def bookmark_params
     params.require(:bookmark).permit(:movie_id, :comment)
   end
 
